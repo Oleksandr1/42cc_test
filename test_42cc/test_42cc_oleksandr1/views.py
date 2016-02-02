45246eb84ce06260a9bae4ee6691a3c0032fb221
Oleksandr Shcherbyna <sanya071186@gmail.com>
2016-02-02 15:34:33 +0200

    Code the model, change the view

--- test_42cc/test_42cc_oleksandr1/views.py
+++ test_42cc/test_42cc_oleksandr1/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
+from models import MyData
 
 # Create your views here.
-my_data = {'name': u'Oleksandr',
-        'second_name': u'Shcherbyna',
-        'email' : u'sanya071186@gmail.com',
-        'jabber' : u'pock@jabber.ua',
-        'skype' : u'sanya071186',
-        'bio' : u'My name is Oleksandr. I`m 29 years old. I`m married and have a doughter',
-        'facebook': u'https://www.facebook.com/profile.php?id=100003368151652',
-        'vk' : u'https://vk.com/id69128840'}
+
 def main(request):
+    my_data = MyData.objects.first()
     return render(request, 'base.html', {'my_data' : my_data})
